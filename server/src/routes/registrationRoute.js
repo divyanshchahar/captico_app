ef004f1f5d09eaeee4cf48883fe98e94e27c5359
divyanshchahar <divyanshchahar@outlook.com>
2024-02-10 22:10:11 +0530

    added registration route

---
+++ server/src/routes/registrationRoute.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const User = require('../schemas/userSchema');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieConfig = require('../config/cookieConfig');
+const tokenConfig = require('../config/tokenConfig');
+
+const router = express.Router();
+
+router.route('/').post(async (req, res) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10);
+		const hashedUser = { ...req.body, password: hashedPassword };
+
+		const user = await User.create(hashedUser);
+
+		const acessToken = jwt.sign(
+			{ id: user._id.toString() },
+			process.env.ACESS_TOKEN_SECRET,
+			tokenConfig.acessToken
+		);
+
+		const refreshToken = jwt.sign(
+			{ id: user._id.toString() },
+			process.env.REFRESH_TOKEN_SECRET,
+			tokenConfig.refreshToken
+		);
+
+		res
+			.cookie(cookieConfig.cookieName, refreshToken, cookieConfig.config)
+			.send({
+				user: { ...user._doc },
+				acessToken: acessToken,
+			})
+			.status(200)
+			.end();
+	} catch (error) {
+		res.status(500).send(error.message).end();
+	}
+});
+
+module.exports = router;
