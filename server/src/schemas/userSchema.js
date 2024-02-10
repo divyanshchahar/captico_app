3a041785c10942d422ee804bf02efc7086447268
divyanshchahar <divyanshchahar@outlook.com>
2024-02-10 18:05:30 +0530

    updated user schema

--- server/src/schemas/userSchema.js
+++ server/src/schemas/userSchema.js
@@ -11,6 +11,11 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	createdAt: {
+		type: Date,
+		default: () => Date.now(),
+		immutable: true,
+	},
 });
 
 module.exports = mongoose.model('users', userSchema);
