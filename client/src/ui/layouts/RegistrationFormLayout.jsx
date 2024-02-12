3fa11f4efc98c0034f7cb1891abb893d5a1287e3
divyanshchahar <divyanshchahar@outlook.com>
2024-02-12 23:51:59 +0530

    completed registration

--- client/src/ui/layouts/RegistrationFormLayout.jsx
+++ client/src/ui/layouts/RegistrationFormLayout.jsx
@@ -1,28 +1,92 @@
+import { useState, useContext } from 'react';
+import apiEndPoints from '../../config/apiEndPoints';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../provider/AuthProvider';
+
 function RegistrationFormLayout() {
+	const { refreshAuth } = useContext(AuthContext);
+	const navigate = useNavigate();
+
+	const [email, setEmail] = useState();
+	const [password1, setPassword1] = useState();
+	const [password2, setPassword2] = useState();
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+
+		const regex = new RegExp('.*@.*.*');
+
+		const passwordsMatch = password1 === password2;
+		const isEmail = regex.test(email);
+
+		if (passwordsMatch & isEmail) {
+			const response = await fetch(apiEndPoints.register, {
+				method: 'POST',
+				credentials: 'include',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ email: email, password: password1 }),
+			});
+
+			const json = await response.json();
+
+			const token = await refreshAuth();
+
+			navigate('/');
+		} else {
+			alert('passwords should match');
+		}
+	};
+
 	return (
 		<form>
 			<div className="m-3">
-				<label className="form-label">User Name</label>
-				<input type="text" className="form-control" />
-			</div>
-
-			<div className="m-3">
 				<label className="form-label">Email address</label>
-				<input type="email" className="form-control" />
+				<input
+					pattern=".*\@.*\.*"
+					type="email"
+					className="form-control"
+					value={email}
+					onChange={(e) => {
+						setEmail(e.target.value);
+					}}
+				/>
 			</div>
 
 			<div className="m-3">
 				<label className="form-label">Password</label>
-				<input type="password" className="form-control" />
+				<input
+					type="password"
+					className="form-control"
+					value={password1}
+					onChange={(e) => {
+						setPassword1(e.target.value);
+					}}
+				/>
 			</div>
 
 			<div className="m-3">
 				<label className="form-label">Retype Password</label>
-				<input type="password" className="form-control" />
+				<input
+					type="password"
+					className="form-control"
+					value={password2}
+					onChange={(e) => {
+						setPassword2(e.target.value);
+					}}
+				/>
 			</div>
 
 			<div className="m-3">
-				<button className="btn btn-primary">Register</button>
+				<button
+					className="btn btn-primary"
+					onClick={(e) => {
+						handleClick(e);
+					}}
+				>
+					Register
+				</button>
 			</div>
 		</form>
 	);
