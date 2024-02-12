145e2715e5726d5d812b056100b2dbb03c855d8f
divyanshchahar <divyanshchahar@outlook.com>
2024-02-12 19:21:39 +0530

    integrated routing provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
+import '../../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 
-import TopNavBar from './ui/layouts/TopNavBar.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import Routing from './routes/Routing.jsx';
 
 function App() {
 	return (
 		<>
-			<TopNavBar />
-			<h1>Test</h1>
+			<BrowserRouter>
+				<Routing />
+			</BrowserRouter>
 		</>
 	);
 }
