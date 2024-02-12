db8dda7b5aa900319e3e3cba98a152e1411d07bd
divyanshchahar <divyanshchahar@outlook.com>
2024-02-13 00:01:48 +0530

    updated layout

--- client/src/ui/layouts/LoginFormLayout.jsx
+++ client/src/ui/layouts/LoginFormLayout.jsx
@@ -1,5 +1,7 @@
 import { useState, useContext } from 'react';
 import AuthContext from '../../provider/AuthProvider';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function LoginFormLayout() {
 	const { login } = useContext(AuthContext);
@@ -7,48 +9,56 @@ function LoginFormLayout() {
 	const [email, setEmail] = useState();
 	const [password, setPassword] = useState();
 
-	const handleClick = (e) => {
+	const navigate = useNavigate();
+
+	const handleClick = async (e) => {
 		e.preventDefault();
-		login(email, password);
+		await login(email, password);
+		navigate('/');
 	};
 
 	return (
-		<form>
-			<div className="m-3">
-				<label className="form-label">Email address</label>
-				<input
-					type="email"
-					className="form-control"
-					id="exampleInputEmail1"
-					value={email}
-					onChange={(e) => {
-						setEmail(e.target.value);
-					}}
-				/>
-			</div>
+		<>
+			<form>
+				<div className="m-3">
+					<label className="form-label">Email address</label>
+					<input
+						type="email"
+						className="form-control"
+						id="exampleInputEmail1"
+						value={email}
+						onChange={(e) => {
+							setEmail(e.target.value);
+						}}
+					/>
+				</div>
 
-			<div className="m-3">
-				<label className="form-label">Password</label>
-				<input
-					type="password"
-					className="form-control"
-					id="exampleInputPassword1"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-			</div>
+				<div className="m-3">
+					<label className="form-label">Password</label>
+					<input
+						type="password"
+						className="form-control"
+						id="exampleInputPassword1"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
 
-			<div className="m-3">
-				<button
-					className="btn btn-primary"
-					onClick={(e) => {
-						handleClick(e);
-					}}
-				>
-					Login
-				</button>
-			</div>
-		</form>
+				<div className="m-3">
+					<button
+						className="btn btn-primary"
+						onClick={(e) => {
+							handleClick(e);
+						}}
+					>
+						Login
+					</button>
+				</div>
+			</form>
+
+			<p>Dont have an account</p>
+			<Link to="/register">Register</Link>
+		</>
 	);
 }
 
