71f8a1431b42e800b71f9d1070c2c80ebfbf0827
divyanshchahar <divyanshchahar@outlook.com>
2024-02-12 10:59:44 +0530

    integrated server changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const registrationRouter = require('./routes/registrationRoute');
+const loginRouter = require('./routes/loginRoute');
+const logoutRouter = require('./routes/logoutRoute');
+const refreshTokenAuthRouter = require('./routes/refreshTokenAuth');
+const coursesRouter = require('./routes/corsesRoute');
+
+require('dotenv').config();
 
 const app = express();
 
-app.listen(4000, () => {
-	console.log('app is listening on port 4000');
+mongoose.connect(process.env.DATABASE).catch((error) => {
+	console.log(error);
+});
+
+app.use(express.json());
+
+app.use('/registration', registrationRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+app.use('/refrshAuth', refreshTokenAuthRouter);
+app.use('/courses', coursesRouter);
+
+app.listen(process.env.PORT, () => {
+	console.log(`app is listening on port ${process.env.PORT}`);
 });
