13f110e80de68a4c4058437031cab4e3daed6f16
divyanshchahar <divyanshchahar@outlook.com>
2024-02-13 14:57:06 +0530

    updated layuot

--- client/src/ui/layouts/CourseFormLayout.jsx
+++ client/src/ui/layouts/CourseFormLayout.jsx
@@ -1,28 +1,33 @@
-function CourseFormLayout() {
+import { useState } from 'react';
+import useMakeAuthRequest from '../../hooks/useMakeAuthRequest';
+
+function CourseFormLayout({ addFunc = () => {} }) {
+	const [name, setName] = useState();
+
+	const makeAuthRequest = useMakeAuthRequest();
+
 	return (
 		<form>
 			<div className="m-3">
 				<label className="form-label">Course Name</label>
-				<input type="text" className="form-control" />
+				<input
+					type="text"
+					className="form-control"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
 			</div>
 
 			<div className="m-3">
-				<label className="form-label">Start Date</label>
-				<input type="date" className="form-control" />
-			</div>
-
-			<div className="m-3">
-				<label className="form-label">End Date</label>
-				<input type="date" className="form-control" />
-			</div>
-
-			<div className="m-3">
-				<label className="form-label">Desription</label>
-				<textarea className="form-control" />
-			</div>
-
-			<div className="m-3">
-				<button className="btn btn-primary">Login</button>
+				<button
+					className="btn btn-primary"
+					onClick={(e) => {
+						e.preventDefault();
+						makeAuthRequest(addFunc, name);
+					}}
+				>
+					Add
+				</button>
 			</div>
 		</form>
 	);
