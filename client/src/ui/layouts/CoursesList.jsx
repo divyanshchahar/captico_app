b3d423a2e51c9dbe19dbf83e4b5a311647b90f93
divyanshchahar <divyanshchahar@outlook.com>
2024-02-13 12:49:28 +0530

    updated layout

--- client/src/ui/layouts/CoursesList.jsx
+++ client/src/ui/layouts/CoursesList.jsx
@@ -1,13 +1,10 @@
-function CoursesList(courses) {
+function CoursesList({ courses }) {
 	return (
 		<ul className="list-group list-group-flush">
 			{courses.map((item) => {
 				return (
 					<li key={item._id} className="list-group-item">
 						<h1>{item.name}</h1>
-						<h6>{`From ${item.startDate} to ${item.endDate}`}</h6>
-						<h6>Description</h6>
-						<p>{item.description}</p>
 
 						<div className="btn-group" role="group" aria-label="Basic example">
 							<button type="button" className="btn btn-primary">
