ab0c2a257dfa0a6d856466106391cc281eda2e0e
divyanshchahar <divyanshchahar@outlook.com>
2024-02-13 16:59:57 +0530

    updated routing

--- client/src/routes/Routing.jsx
+++ client/src/routes/Routing.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import LoginFormsLayout from '../ui/layouts/LoginFormLayout';
 import RegistrationFormLayout from '../ui/layouts/RegistrationFormLayout';
 import TopNavBar from '../ui/layouts/TopNavBar';
+import CoursesPage from '../ui/pages/CoursesPage';
 
 function Routing() {
 	return (
@@ -10,6 +11,7 @@ function Routing() {
 				<Route path="/" element={<TopNavBar />} />
 				<Route path="login" element={<LoginFormsLayout />} />
 				<Route path="register" element={<RegistrationFormLayout />} />
+				<Route path="courses" element={<CoursesPage />} />
 			</Routes>
 		</>
 	);
