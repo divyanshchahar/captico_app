33455b584a97c2a90e989b7479c299196781516e
divyanshchahar <divyanshchahar@outlook.com>
2024-02-13 17:44:20 +0530

    updated routing

--- client/src/routes/Routing.jsx
+++ client/src/routes/Routing.jsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
-import LoginFormsLayout from '../ui/layouts/LoginFormLayout';
 import RegistrationFormLayout from '../ui/layouts/RegistrationFormLayout';
-import TopNavBar from '../ui/layouts/TopNavBar';
 import CoursesPage from '../ui/pages/CoursesPage';
+import HomePage from '../ui/pages/HomePage';
+import RootPage from '../ui/pages/RootPage';
+import LoginFormLayout from '../ui/layouts/LoginFormLayout';
 
 function Routing() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<TopNavBar />} />
-				<Route path="login" element={<LoginFormsLayout />} />
-				<Route path="register" element={<RegistrationFormLayout />} />
-				<Route path="courses" element={<CoursesPage />} />
+				<Route path="/" element={<RootPage />}>
+					<Route index element={<HomePage />} />
+					<Route path="register" element={<RegistrationFormLayout />} />
+					<Route path="courses" element={<CoursesPage />} />
+					<Route path="login" element={<LoginFormLayout />} />
+				</Route>
 			</Routes>
 		</>
 	);
