97e4ddbacc1ed3cb93e1072827aaddfef55ec871
divyanshchahar <divyanshchahar@outlook.com>
2024-02-13 16:57:45 +0530

    updated layout

--- client/src/ui/layouts/CoursesList.jsx
+++ client/src/ui/layouts/CoursesList.jsx
@@ -1,7 +1,12 @@
 import useMakeAuthRequest from '../../hooks/useMakeAuthRequest';
 
 // eslint-disable-next-line react/prop-types
-function CoursesList({ courses = [], delFunc = () => {} }) {
+function CoursesList({
+	courses = [],
+	delFunc = () => {},
+	toggleEditing = () => {},
+	setEditId,
+}) {
 	const makeAuthRequest = useMakeAuthRequest();
 
 	return (
@@ -12,7 +17,14 @@ function CoursesList({ courses = [], delFunc = () => {} }) {
 						<h1>{item.name}</h1>
 
 						<div className="btn-group" role="group" aria-label="Basic example">
-							<button type="button" className="btn btn-primary">
+							<button
+								type="button"
+								className="btn btn-primary"
+								onClick={() => {
+									setEditId(item._id);
+									toggleEditing(true);
+								}}
+							>
 								<i className="bi bi-pen-fill"></i>
 							</button>
 							<button
